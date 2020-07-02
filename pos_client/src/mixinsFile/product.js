1ad21c91da900e8ddbb601bceb7d01df347cc74e
yelinIT <yelinphyocu@gmail.com>
2020-07-03 00:24:28 +0630

    no message

--- pos_client/src/mixinsFile/product.js
+++ pos_client/src/mixinsFile/product.js
@@ -40,6 +40,19 @@ export const Product = {
   created() {
     this.imageUploadUrl = this.baseUrl + '/shared/productIMG'
     this.getProductList()
+    if (this.$route.query.data === 'add') {
+      this.activeName = 'add'
+    }
+  },
+  computed: {
+    getTab() {
+      return this.$store.getters.newTab
+    }
+  },
+  watch: {
+    getTab() {
+      console.log('tab', this.$store.getters.newTab)
+    }
   },
   methods: {
     async querySearchAsync(queryString, cb) {
